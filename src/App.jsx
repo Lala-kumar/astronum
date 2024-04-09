3449ad01a03b49b4e4c483f44a4bcf734cc358b1
Lala Kumar <lalak4099@gmail.com>
2024-04-09 17:32:06 +0530

    added signup page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Home from "./component/home/Home";
 import Rating from "./component/rating/Rating";
 import MyAccount from "./component/account/MyAccount";
 import Notification from "./component/notification/Notification";
-import Login from './component/login/Login'
+import Login from "./component/login/Login";
+import SignUp from "./component/signup/Signup";
 
 const App = () => {
   const router = createBrowserRouter([
@@ -13,6 +14,7 @@ const App = () => {
     { path: "/account", element: <MyAccount /> },
     { path: "/notification", element: <Notification /> },
     { path: "/login", element: <Login /> },
+    { path: "/signup", element: <SignUp /> },
   ]);
 
   return (
--- src/component/signup/Signup.jsx
+++ src/component/signup/Signup.jsx
@@ -1,18 +1,17 @@
-import React from "react";
+import SignupSvg from "../../assets/signup.svg";
 import Layout from "../layout/Layout";
-import loginSvg from "../../assets/login.svg";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+export default function SignUp() {
   return (
     <Layout>
       <main className="h-full pt-20 pb-20 mx-auto px-10 flex flex-col md:px-28 bg-gray-100">
         <div
-          className={`flex justify-evenly w-full items-center flex-col lg:flex-row basis-full`}
+          className={`flex justify-evenly w-full items-center flex-col lg:flex-row-reverse basis-full`}
         >
           <div className="flex items-center justify-center">
             <div className="relative w-80 h-80 lg:w-96 lg:h-96">
-              <img alt="login-img" src={loginSvg} />
+              <img alt="signup-img" src={SignupSvg} />
             </div>
           </div>
           <div className="w-full sm:max-w-sm mx-5">
@@ -20,10 +19,37 @@ const Login = () => {
               <label htmlFor="email" className="block mb-4">
                 <input
                   className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-pink-400"
+                  id="name"
+                  name="name"
+                  type="name"
+                  placeholder="Name"
+                />
+              </label>
+              <label htmlFor="number" className="block mb-4">
+                <input
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-pink-400"
+                  id="number"
+                  name="number"
+                  type="text"
+                  placeholder="Contact No."
+                />
+              </label>
+              <label htmlFor="address" className="block mb-4">
+                <input
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-pink-400"
+                  id="address"
+                  name="address"
+                  type="address"
+                  placeholder="Address"
+                />
+              </label>
+              <label htmlFor="email" className="block mb-4">
+                <input
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-pink-400"
                   id="email"
                   name="email"
                   type="email"
-                  placeholder="Email address"
+                  placeholder="Email "
                 />
               </label>
 
@@ -37,28 +63,21 @@ const Login = () => {
                 />
               </label>
 
-              <a
-                href="#"
-                className="block text-sm text-neutral-600 hover:text-pink-400 mb-6 "
-              >
-                Forgot password?
-              </a>
-
-              <div className="flex flex-col items-center  lg:justify-between">
+              <div className="text-center lg:text-left">
                 <button
                   type="submit"
                   className="w-full  bg-pink-400 text-white py-2 px-6 rounded-md shadow-md hover:bg-pink-500 uppercase mb-4 lg:mb-0"
                 >
-                  Login
+                  Sign-up
                 </button>
 
-                <p className="text-sm font-semibold mt-2 mb-0 text-neutral-600">
-                  Don't have an account?{" "}
+                <p className="text-sm font-semibold mt-2 pt-1 mb-0 space-x-1 text-neutral-600">
+                  <span>Do you already have an account?</span>
                   <Link
-                    to={"/signup"}
+                    to={"/login"}
                     className="text-pink-500 hover:text-pink-600 transition duration-200 ease-in-out"
                   >
-                    <span>Register</span>
+                    <span>Log In</span>
                   </Link>
                 </p>
               </div>
@@ -68,5 +87,4 @@ const Login = () => {
       </main>
     </Layout>
   );
-};
-export default Login;
+}
